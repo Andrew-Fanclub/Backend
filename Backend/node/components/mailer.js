247dd5d929b1e90544f4a108fd150d1689e6d28b
bradosia <32529373+bradosia@users.noreply.github.com>
2022-03-24 23:46:06 -0700

    Simple mysql database creation
    Changed the server from CommonJS to ES module.

--- Backend/node/components/mailer.js
+++ Backend/node/components/mailer.js
@@ -4,32 +4,42 @@
 // side note need to wait for catherine to finish up frontend so I can connect it
 // also need to see if heroku will allow port-forwarding for mailjet api
 
-import { Mailer } from 'nodemailer-react';
+import {
+  Mailer
+} from 'nodemailer-react';
 
 const transport = {
-    host: 'afcplushies@gmail.com',
-    secure: true,
-    auth: {user: 'afcplushies@gmail.com', pass: 'af2plush!3$'},
+  host: 'afcplushies@gmail.com',
+  secure: true,
+  auth: {
+    user: 'afcplushies@gmail.com',
+    pass: 'af2plush!3$'
+  },
 }
 
 const defaults = {
-    from: 'body.req.email' + ' ' + 'body.req.name',
+  from: 'body.req.email' + ' ' + 'body.req.name',
 }
 
 export const email = () => ({
-    subject: body.req.email,
-    body: (
-        'body.req.message'
-    )
+  subject: body.req.email,
+  body: (
+    'body.req.message'
+  )
 })
 
-export const mailer = Mailer(
-    { transport, defaults },
-    { email }
-)
+export const mailer = Mailer({
+  transport,
+  defaults
+}, {
+  email
+})
 
 mailer.send('email', {
-    to: 'afcplushies@gmail.com'
+  to: 'afcplushies@gmail.com'
 })
 
-module.exports = { Mailer };
+export {
+  Mailer as
+  default
+};
--- README.md
+++ README.md
@@ -1,4 +1,11 @@
 # Backend
+3/24/2022 From Brad:
+BIG BREAKING CHANGES!!! Changed the server from CommonJS to ES module.
+
+See https://blog.logrocket.com/commonjs-vs-es-modules-node-js/#:~:text=ES%20modules%20are%20the%20standard,encapsulating%20JavaScript%20code%20for%20reuse.
+
+ES modules use import/export syntax
+
 
 New additions to this most recent version:
 - Node files made to run background processes for frontend (Still in the works it's not cleaned up yet)
